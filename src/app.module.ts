706d605e618871e1be4824b840691e8fbd1ab757
Haris Shoaib <haris005@gmail.com>
2022-08-31 17:08:31 -0400

    Generated resources for User & Reservation models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UsersModule, ReservationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
