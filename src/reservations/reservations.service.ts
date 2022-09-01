c1e7f745d84f8e7050455b158204f0398ec5c43c
Haris Shoaib <haris005@gmail.com>
2022-09-01 10:51:30 -0400

    Implemented dates overlap check

--- src/reservations/reservations.service.ts
+++ src/reservations/reservations.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
-import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class ReservationsService {
 
   constructor(private prisma: PrismaService) {}
 
-  create(createReservationDto: CreateReservationDto) {
+  async create(createReservationDto: CreateReservationDto) {
+    const reqCheckInDate = new Date(createReservationDto.checkInDate);
+    const reqCheckOutDate = new Date(createReservationDto.checkOutDate);
+    const reservations = await this.findAll();
+    for (let r of reservations) {
+      const rCheckInDate = new Date(r.checkInDate);
+      const rCheckOutDate = new Date(r.checkOutDate);
+      if ((reqCheckInDate == rCheckInDate && reqCheckOutDate == rCheckOutDate)
+       || (reqCheckInDate > rCheckInDate && reqCheckInDate < rCheckOutDate)
+       || (reqCheckOutDate > rCheckInDate && reqCheckOutDate < rCheckOutDate)
+       || (reqCheckInDate < rCheckInDate && reqCheckOutDate > rCheckOutDate)) {
+        throw new HttpException({
+          status: HttpStatus.FORBIDDEN,
+          error: 'Reservation dates overlap with ' + 'reservation ' + r.id + ' starting on ' + rCheckInDate + ' and ending on ' + rCheckOutDate,
+        }, HttpStatus.FORBIDDEN);
+      }
+    }
     return this.prisma.reservation.create({ data: createReservationDto });
   }
 
